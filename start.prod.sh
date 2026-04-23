73a81f3c282acfa6da044d87e19f18fa0d8f1914
Escanor-dev95 <floflobarroso@gmail.com>
2026-04-23 10:03:43 +0200

    update script

--- start.prod.sh
+++ start.prod.sh
@@ -4,10 +4,6 @@ set -e
 
 docker info >/dev/null 2>&1 || { echo "❌ Docker n'est pas démarré"; exit 1; }
 
-echo "🚀 Lancement de l'environnement de prod..."
-cp .env.prod.example .env
-mkdir -p backend/docker_data/prod
-
 echo "🏗 Build et démarrage de tous les services..."
 docker compose -f docker-compose.prod.yml up -d --build
 
